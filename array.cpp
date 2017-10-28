6cd2296adee35d985f0bd3642482c1f0b83025a4
MradulSaxena <mradulsmiles@gmail.com>
2017-10-28 09:19:02 -0700

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -26,9 +26,35 @@ int findone(const vector<int> a) {
     return ones;
 }
 
+//
+// Kadane's Algorithm is also known as maximum subarray
+//
+int kadane_algorithm (vector <int>&arr)
+{
+    int max_ending_here = 0;
+    int max_so_far = 0;
+    
+    for (int i=0;i<arr.size();i++) {
+        
+        max_ending_here += arr[i];
+        
+        if (max_ending_here < 0) {
+            max_ending_here = 0;
+        }
+        
+        if (max_so_far < max_ending_here) {
+            max_so_far = max_ending_here;
+        }
+    }
+    
+    return max_so_far;
+}
 
 
-int findmissing(vector<int>&arr, int start, int high,const int first_val) {
+//
+// find a missing number from a sorted array
+//
+int findmissing(vector<int>&arr, int start, int high, const int first_val) {
     
     if (start>high) {
         return high+first_val+1;
@@ -48,29 +74,131 @@ int findmissing(vector<int>&arr, int start, int high,const int first_val) {
 }
 
 //
-// Kadane's Algorithm is also known as maximum subarray
+// Subarray with given sum (Google, Facebook, Apple, Amazon)
 //
-int kadane_algorithm (vector <int>&arr)
+void subarray_with_given_sum (vector<int>&arr, int sum)
 {
-    int sum = INT_MIN;
-    int sum_so_far = INT_MIN;
+    int index_low = 0;
+    int index_high = 0;
+    int curr_sum = 0;
     
+    while (index_high<arr.size() && curr_sum != sum) {
+        
+        while (curr_sum > sum && index_low <arr.size()) {
+            curr_sum = curr_sum - arr[index_low];
+            index_low++;
+        }
+        
+        curr_sum += arr[index_high];
+        index_high++;
+    }
     
-    
-    return sum;
+    cout << "\nSubarray found at " <<index_low<< " "<< index_high-1 <<endl;
 }
 
+//
+// Sort an array of 0s 1s and 2s
+//
+void sort_array_0_1_2 (vector<int> &arr) {
+    
+    int zero_count = 0;
+    int ones_count = 0;
+    int twos_count = 0;
+    
+    for (int i : arr) {
+        if (i == 0) {
+            zero_count++;
+        } else if (i == 1) {
+            ones_count++;
+        } else {
+            twos_count++;
+        }
+    }
+    
+    for (int i = 0;i<arr.size();i++) {
+        if (zero_count != 0) {
+            arr[i] = 0;
+            zero_count--;
+        } else if (ones_count != 0) {
+            arr[i]  = 1;
+            ones_count--;
+        } else {
+            arr[i]  = 2;
+            twos_count--;
+        }
+    }
+}
+
+//
+// find the point where left sum is equal to the right sum
+// idea is to keep track of right sum and left sum at each index
+//
+void find_equilibrium_point (vector<int> arr) {
+    int array_size = static_cast<int>(arr.size());
+    int sum = 0;
+    
+    for (int i:arr) {
+        sum+=i;
+    }
+    
+    int left_sum = 0;
+    
+    for (int i=0;i<array_size;i++) {
+        // This holds the right sum for ith index
+        sum = sum - arr[i];
+        
+        if (left_sum == sum) {
+            cout << "Solution found at " << i <<endl;
+            return;
+        }
+        
+        left_sum += arr[i];
+    }
+}
+
+//
+//  max_increasing_subsequence : find the maximum sum from increasing numbers
+//
+int max_increasing_subsequence (vector<int> arr) {
+    
+    return 0;
+}
 
 void test_array (void) {
-//    vector<int> arr = {4,5,6,7,8,10,11,12};
-//    //vector<int> arr = {1,2,3,4,6,7,8,9};
-//    cout << "val is "<< findmissing(arr, 0, static_cast<int>(arr.size()-1), arr[0])<< endl;
-//
-//
-//    vector<int>a = {1,1,1,2,2,3,2,3,3,5};
-//    cout<< "number is "<< findone(a)<<endl;
-    
-    vector<int> array = {-2, -3, 4, -1, -2, 1, 5, -3};
-    cout << "maximum_subarray sum is " << kadane_algorithm(array)<<endl;
-}
 
+    cout << "\nFind the number appearing only once in the array\n";
+    vector<int>array1 = {1,1,1,2,2,3,2,3,3,5};
+    cout<< "number is "<< findone(array1)<<endl;
+    
+    cout << "\nFind the maximum subarray (Kadane Algorithm)\n";
+    vector<int> array2 = {-2, -3, 4, -1, -2, 1, 5, -3};
+    cout << "maximum_subarray sum is " << kadane_algorithm(array2)<<endl;
+    
+    cout << "\nFind the subarray with the given sum\n";
+    vector<int> array3 = {15, 2, 4, 8, 9, 5, 10, 23};
+    subarray_with_given_sum(array3, 23);
+    
+    cout << "\nSort an array of 0s 1s and 2s\n";
+    vector<int>array4 = {1,2,1,0,2,1,0,2,0,1};
+    sort_array_0_1_2(array4);
+    
+    for (int i: array4) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    cout << "\nEquilibrium Point (Sum of left = Sum of right)\n";
+    vector<int> equili = {-7, 1, 5, 2, -4, 3, 0};
+    find_equilibrium_point(equili);
+    cout << endl;
+    
+    cout << "\nMaximum Sum Increasing Subsequence(Not LIS) \n";
+    cout << "Maximum Sum Increasing Subsequence is " <<
+            max_increasing_subsequence(array3);
+    cout << endl;
+    
+    //    vector<int> arr = {4,5,6,7,8,10,11,12};
+    //    //vector<int> arr = {1,2,3,4,6,7,8,9};
+    //    cout << "val is "<<
+    //        findmissing(arr, 0, static_cast<int>(arr.size()-1), arr[0])<< endl;
+}
