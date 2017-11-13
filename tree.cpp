56404296ca02edcc5cfe6274f02612bbfe96354f
MradulSaxena <mradulsmiles@gmail.com>
2017-11-12 20:34:39 -0800

    adding a TC

--- tree.cpp
+++ tree.cpp
@@ -33,14 +33,39 @@ void print_left_view (node_t *root, int level, int *max)
     print_left_view(root->right, level+1, max);
 }
 
+//
+//check_bst: checks if the tree is a BST
+//
+bool check_bst (node_t *root)
+{
+    static node_t *prev = nullptr;
+    
+    if (!root) {
+        return true;
+    }
+    if (!check_bst(root->left)) {
+        return false;
+    }
+    
+    if (prev && prev->data >= root->data) {
+        return false;
+    }
+    
+    prev = root;
+    return (check_bst(root->right));
+
+}
 void test_tree (void)
 {
     int max_level = INT_MIN;
     node_t *root = newNode(12);
-    root->left = newNode(10);
+    root->left = newNode(9);
     root->right = newNode(30);
     root->right->left = newNode(25);
     root->right->right = newNode(40);
     
+    
     print_left_view(root, 0, &max_level);
+    cout << "\nTree is BST "<< check_bst(root);
+    
 }
