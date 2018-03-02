501008f4ebd7024328d013555145700676892e3b
MradulSaxena <mradulsmiles@gmail.com>
2018-03-01 22:25:25 -0800

    adding placeholder for new impl.

---
+++ hash_table.cpp
@@ -0,0 +1,30 @@
+//
+//  hash_table.cpp
+//  XCODE
+//
+//  Created by Mradul Saxena on 1/5/18.
+//  Copyright © 2018 Mradul Saxena. All rights reserved.
+//
+
+#include "common_incl.h"
+
+#define TABLE_SIZE 1000
+
+hash_node_t *table[TABLE_SIZE];
+
+int hash_search (int key)
+{
+    return 0;
+}
+
+int hash_get (int key)
+{
+    return 0;
+}
+
+int hash_put (int key, int value)
+{
+    return 0;
+}
+
+
